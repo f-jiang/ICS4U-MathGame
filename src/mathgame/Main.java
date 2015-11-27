950364260557d44bb480ea7178418d71088e4bf5
Feilan Jiang <feilanjiang@gmail.com>
2015-11-26 19:46:43 -0500

    Restructure packages

--- src/mathgame/Main.java
+++ src/mathgame/Main.java
@@ -3,9 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mathgame.main;
-
-import mathgame.gui.Screens;
+package mathgame;
 
 /**
  *
