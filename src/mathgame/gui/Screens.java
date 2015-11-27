3b3c789bbf2d29963f1eb5fe5f79e63e215c1c0c
Feilan Jiang <feilanjiang@gmail.com>
2015-11-26 20:17:18 -0500

    Revert "Restructure packages"
    This reverts commit 950364260557d44bb480ea7178418d71088e4bf5.

--- src/mathgame/gui/Screens.java
+++ src/mathgame/gui/Screens.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mathgame;
+package mathgame.gui;
 
 import java.awt.CardLayout;
 
--- src/mathgame/main/Main.java
+++ src/mathgame/main/Main.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mathgame;
+package mathgame.main;
+
+import mathgame.gui.Screens;
 
 /**
  *
